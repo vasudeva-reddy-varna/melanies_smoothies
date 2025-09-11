b0853861dad3a5a2b9091456947745e23678220c
vasudeva-reddy-varna <vasudeva.varna@pibythree.com>
2025-09-11 13:13:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,10 @@ time_to_insert=st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered '+Name_on_order+'!', icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df=st.dataframe(smoothiefroot_response.json(), use_container_width=True)
+
+if ingredients_list:
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        st.subheader(fruit_chosen+"nutrition information")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        sf_df=st.dataframe(smoothiefroot_response.json(), use_container_width=True)
