36fd27c1df6c6809caaf858d7b88d86eaf441a5d
vasudeva-reddy-varna <vasudeva.varna@pibythree.com>
2025-09-14 16:52:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,9 @@ Name_on_order = st.text_input("Name on Smoohie")
 st.write("Name on your smoothie will be:", Name_on_order)
 cnx= st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list =  st.multiselect(
                 'Choose upto 5 ingredients', 
