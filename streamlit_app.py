42b5cd7b722c91799dc7f60bd84ca0502882c936
vasudeva-reddy-varna <vasudeva.varna@pibythree.com>
2025-09-14 17:05:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
 
-        st.write(ingredients_string)
+        # st.write(ingredients_string)
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """','""" + Name_on_order + """')"""
 time_to_insert=st.button('Submit Order')
